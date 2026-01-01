9fb43499e557d8e3bf4d342ca3bac9ef8d50383f
Manan <imananop@gmail.com>
2026-01-01 21:54:44 +0530

    product form

--- components/AddProductForm.jsx
+++ components/AddProductForm.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+"use client";
+
+import React, { useState } from 'react'
+import { Input } from './ui/input'
 
 const AddProductForm = ({user}) => {
+
+  const [url, setUrl] = useState("");
+
   return (
     <form>
       <div className='flex flex-col sm:flex-row gap-2'>
-        
+        <Input type={"url"} value={url} onChange={(e) => setUrl(e.target.value)} />
       </div>
     </form>
   )
