dd165e7ec3f3794cb687d681059db6e72b2a6017
Manan <imananop@gmail.com>
2026-01-01 21:58:30 +0530

    ui form

--- components/AddProductForm.jsx
+++ components/AddProductForm.jsx
@@ -6,11 +6,22 @@ import { Input } from './ui/input'
 const AddProductForm = ({user}) => {
 
   const [url, setUrl] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {};
 
   return (
-    <form>
+    <form onSubmit={handleSubmit} className='w-full max-w-2xl mx-auto '>
       <div className='flex flex-col sm:flex-row gap-2'>
-        <Input type={"url"} value={url} onChange={(e) => setUrl(e.target.value)} />
+        <Input 
+          type={"url"} 
+          value={url} 
+          onChange={(e) => setUrl(e.target.value)} 
+          placeholder="Enter product URL (Amazon, Flipkart, etc.)" 
+          className={"h-12 text-base"} 
+          required
+          disabled={loading}
+        />
       </div>
     </form>
   )
