dc361eefa6e3d9ac11d7227dbc5088ba74a96daf
Manan <imananop@gmail.com>
2026-01-02 21:42:45 +0530

    signin

---
+++ components/AuthModal.js
@@ -0,0 +1,28 @@
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+export function AuthModal({isOpen, onClose}) {
+  return (
+    <Dialog open={isOpen} onOpenChange={onClose}>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Edit profile</DialogTitle>
+            <DialogDescription>
+              Make changes to your profile here. Click save when you&apos;re
+              done.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+
+    </Dialog>
+  )
+}
