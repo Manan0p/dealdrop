1786d1cda3a9784979fe98271922378f677ae9ca
Manan <imananop@gmail.com>
2026-01-03 23:13:09 +0530

    scrape action

--- app/actions.js
+++ app/actions.js
@@ -1,5 +1,6 @@
 "use server"
 
+import { scrapeProduct } from "@/lib/firecrawl";
 import { createClient } from "@/utils/supabase/server";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
@@ -9,4 +10,27 @@ export async function signOut() {
     await supabase.auth.signOut();
     revalidatePath("/");
     redirect("/");
+}
+export async function addProduct(formData) {
+    const url = formData.get("url");
+
+    if (!url) {
+        return{ error: "URL is required" };
+    }
+
+    try {
+        const supabase = await createClient();
+        const {
+            data: { user },
+        } = await supabase.auth.getUser();
+
+        if (!user) {
+            return { error: "User not authenticated" };
+        }
+
+        const productData = await scrapeProduct(url);
+        
+    } catch (error) {
+        
+    }
 }
